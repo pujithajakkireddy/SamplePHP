87bb344ded2c05dea7f05cd42b042131a4cc0c63
Pujitha <pujithajakkireddy@gmail.com>
2025-08-14 19:58:46 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Welcome to my website!"; ?>
+    </h1>
+    <p>This page was generated with PHP.</p>
+</body>
+</html>
